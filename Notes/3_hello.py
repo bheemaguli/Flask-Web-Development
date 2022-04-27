af331f0b3a97f5d99ab0b38df7356f3d2b75d17f
Sumanth Kashyap <sumanth.bhi@gmail.com>
2022-04-27 14:22:51 +0530

    Templates

--- Notes/3_hello.py
+++ Notes/3_hello.py
@@ -13,5 +13,13 @@ def index():
 def user(name):
     return render_template('user.html', name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html', e=e)
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html', e=e)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
