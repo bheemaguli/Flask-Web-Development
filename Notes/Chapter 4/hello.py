5ec0273e40edde4dc7fc298e506405136d37b996
Sumanth Kashyap <sumanth.bhi@gmail.com>
2022-05-03 17:11:24 +0530

    Web Forms

--- Notes/Chapter 4/hello.py
+++ Notes/Chapter 4/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -16,10 +16,18 @@ app.config['SECRET_KEY'] = 'sumo is coding'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
     return render_template('index.html',
-                            current_time=datetime.utcnow())
+                            form=form,
+                            name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
