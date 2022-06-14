3dac79cba872943656aacec84c0a374c2d21a04b
bheemaguli <sumanth.bhi@gmail.com>
2022-06-14 17:48:06 +0530

    Large Appliction Structure

--- Notes/Chapter 6/hello.py
+++ Notes/Chapter 6/hello.py
@@ -26,7 +26,7 @@ app.config['SQLALCHEMY_DATABASE_URI']           = \
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']    = False
 
-app.config['MAIL_SERVER']='smtp.vanward.co'
+app.config['MAIL_SERVER']=os.environ.get('MAIL_SERVER')
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = False
@@ -34,7 +34,7 @@ app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[FLASK LEARNING]'
-app.config['FLASKY_MAIL_SENDER']         = 'sumanth@vanward.co'
+app.config['FLASKY_MAIL_SENDER']         = os.environ.get('FLASKY_MAIL_SENDER')
 app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 mail    = Mail(app)  # generate this after the above configs!!!
 
