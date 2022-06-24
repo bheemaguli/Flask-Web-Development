f67942204a375fb121f9535960bb60049a03f039
bheemaguli <sumanth.bhi@gmail.com>
2022-06-24 18:04:27 +0530

    User Authentication

--- Notes/Chapter 8/app/models.py
+++ Notes/Chapter 8/app/models.py
@@ -2,6 +2,13 @@ from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import login_manager
 from flask_login import UserMixin
+from flask import current_app
+from . import db
+
+# Deprecated 
+# from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+import jwt # pyjwt
+import datetime
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
@@ -26,7 +33,88 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    confirm = db.Column(db.Boolean, default=False)
+    
+    # def generate_confirmation_token(self, expiration=3600):
+    #     s = Serializer(current_app.config['SECRET_KEY'], expiration)
+    #     return s.dumps({'confirm':self.id}).decode('utf-8')
+    
+    # def confirm(self, token):
+    #     s = Serializer(current_app.config['SECRET_KEY'])
+    #     try:
+    #         data = s.loads(token.encode('utf-8'))
+    #     except:
+    #         return False
+    #     if data.get('confirm') != self.id:
+    #         return False
+    #     self.confirmed = True
+    #     db.session.add(self)
+    #     return True
+    
+    def generate_confirmation_token(self, expiration=600):
+        reset_token = jwt.encode(
+            {
+                "confirm": self.id,
+                "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+                       + datetime.timedelta(seconds=expiration)
+            },
+            current_app.config['SECRET_KEY'],
+            algorithm="HS256"
+        )
+        return reset_token
 
+    def confirm(self, token):
+        try:
+            data = jwt.decode(
+                token,
+                current_app.config['SECRET_KEY'],
+                algorithms="HS256"
+            )
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        return True
+    
+    # def generate_confirmation_token(self, expiration=3600):
+    #     """ Generate for mailbox validation JWT (json web token)"""
+    #     #  Signature algorithm 
+    #     header = {
+    #     'alg': 'HS256',}
+    #     #  The key used for the signature 
+    #     key = current_app.config['SECRET_KEY']
+    #     #  Data load to be signed 
+    #     data = {'confirm': self.id, 'exp':expiration}
+        
+    #     return jwt.encode(header=header, payload=data, key=key)
+    #     reset_token = jwt.encode(
+    #         {
+    #             "confirm": self.id,
+    #             "exp": datetime.datetime.now(tz=datetime.timezone.utc)
+    #                    + datetime.timedelta(seconds=expiration)
+    #         },
+    #         current_app.config['SECRET_KEY'],
+    #         algorithm="HS256"
+    #     )
+
+    # def confirm(self, token):
+    #     """ Used to verify user registration and change password or mailbox token,  And complete the corresponding confirmation operation """
+    #     key = current_app.config['SECRET_KEY']
+
+    #     try:
+    #         data = jwt.decode(token, key)
+    #         print(data)
+    #     except JoseError:
+    #         return False
+        
+    #     if data.get('confirm') != self.id:
+    #         return False
+    #     self.confirmed = True
+    #     db.session.add(self)
+    #     return True
 
 class Role(db.Model):
     __tablename__ = 'roles'
---
+++ Notes/Chapter 8/migrations/versions/64f5b057ed18_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 64f5b057ed18
+Revises: a84c3ad45fee
+Create Date: 2022-06-24 15:59:32.250971
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '64f5b057ed18'
+down_revision = 'a84c3ad45fee'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
